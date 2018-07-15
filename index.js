a73d434e4a06637e5e1abfc347234dcbab227537
Tao <werewaswas@live.com>
2018-07-15 03:21:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ function congratulateCustomer(){
 
 function attemptTwoFavoriteCustomers(){
     let favoriteCustomer = 'bar'
-    favoriteCustomer = 'not bar'
+    let favoriteCustomer = 'not bar'
 }
